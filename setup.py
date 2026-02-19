3577127067de7fd34ee338583233c88f8183c702
Shabbir Bhori <bhorishabbir82@gmail.com>
2026-02-19 22:27:57 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="shabbir bhori",
+    author_email="bhorishabbir5253@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
